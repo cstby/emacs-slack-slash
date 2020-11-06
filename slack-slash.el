39b101342b67ca7916eefab9ebebbf522c7d32bb
Carl Steib <carl.steib@gmail.com>
2020-11-06 12:38:32 -0800

    Add feedback after command execution

--- slack-slash.el
+++ slack-slash.el
@@ -76,33 +76,38 @@
     (string-trim (buffer-string))))
 
 (defmacro clj-slack-setup (&rest plist)
-  (let ((fun-name (plist-get plist :fun-name))
-        (command (plist-get plist :command))
-        (channel-name (plist-get plist :channel-name))
-        (user-name (plist-get plist :user-name))
-        (token (plist-get plist :token))
-        (external-url (plist-get plist :external-url)))
-    `(defun ,fun-name ()
-       (interactive)
-       (cl-labels ((slash-cmd (cmd)
-                              (let ((query-data (make-hash-table :test 'equal)))
-                                (puthash 'token ,token query-data)
-                                (puthash 'command ,command query-data)
-                                (puthash 'channel_name ,channel-name query-data)
-                                (puthash 'user_name ,user-name query-data)
-                                (puthash 'text cmd query-data)
-                                (web-http-post
-                                 (lambda (con header data))
-                                 :url ,external-url
-                                 :data query-data
-                                 ))))
-         (save-excursion
-           (end-of-defun)
-           (backward-sexp)
-           (let ((sexp (thing-at-point 'sexp)))
-             (slash-cmd sexp))
-           )))
-    ))
   "Takes a configuration PLIST and produces a function."
+	(let ((fun-name (plist-get plist :fun-name))
+				(command (plist-get plist :command))
+				(channel-name (plist-get plist :channel-name))
+				(user-name (plist-get plist :user-name))
+				(token (plist-get plist :token))
+				(external-url (plist-get plist :external-url)))
+		`(progn
+			 (defun ,fun-name ()
+				 (interactive)
+				 (cl-labels ((slash-cmd (cmd)
+																(let ((query-data (make-hash-table :test 'equal)))
+																	(puthash 'token ,token query-data)
+																	(puthash 'command ,command query-data)
+																	(puthash 'channel_name ,channel-name query-data)
+																	(puthash 'user_name ,user-name query-data)
+																	(puthash 'text cmd query-data)
+																	(web-http-post
+																	 (lambda (con header data))
+																	 :url ,external-url
+																	 :data query-data))))
+					 (save-excursion
+						 (end-of-defun)
+						 (backward-sexp)
+						 (let ((sexp (thing-at-point 'sexp)))
+							 (slash-cmd sexp))))
+				 (message "Sent to slack!"))
+			 (define-eval-sexp-fu-flash-command ,fun-name
+				 (eval-sexp-fu-flash (save-excursion
+															 (end-of-defun)
+															 (beginning-of-defun)
+															 (bounds-of-thing-at-point 'sexp)))))))
+
 (provide 'slack-slash)
 ;;; slack-slash.el ends here
